b3bc300f63a2d16361bb97d81dcc55c2d258d65a
82159 - Jose Nuñez Scarano <josen9999@gmail.com>
2026-09-13 02:43:28 +0000

    feat: Implementacion logica para exchange (MessageMiddlewareExchangeRabbitMQ).

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -59,19 +59,19 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
     #Si ocurre un error interno que no puede resolverse eleva MessageMiddlewareMessageError.
     def start_consuming(self, on_message_callback):
 
-            try:
+        try:
 
-                self.channel.basic_consume(queue=self.queue_name, 
-                                           on_message_callback=self.wrap_callback(on_message_callback))
+            self.channel.basic_consume(queue=self.queue_name, 
+                                        on_message_callback=self.wrap_callback(on_message_callback))
 
-                self.channel.start_consuming()
+            self.channel.start_consuming()
 
-            except pika.exceptions.AMQPConnectionError as e:
-                raise MessageMiddlewareDisconnectedError() from e 
-                
-            except pika.exceptions.AMQPError as e:
-                raise MessageMiddlewareMessageError() from e 
-                # resta ver si hay un caso genuino de error que no es error a considerar
+        except pika.exceptions.AMQPConnectionError as e:
+            raise MessageMiddlewareDisconnectedError() from e 
+            
+        except pika.exceptions.AMQPError as e:
+            raise MessageMiddlewareMessageError() from e 
+            # resta ver si hay un caso genuino de error que no es error a considerar
      
     #Si se estaba consumiendo desde la cola/exchange, se detiene la escucha. Si
     #no se estaba consumiendo de la cola/exchange, no tiene efecto, ni levanta
@@ -123,4 +123,120 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
     def __init__(self, host, exchange_name, routing_keys):
-        pass
+
+        try:
+
+            self.routing_keys = routing_keys
+
+            #conexion al host
+            self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=host))
+
+            #canal
+            self.channel = self.connection.channel()
+
+            #exchange
+            self.exchange_name = exchange_name
+
+            self.channel.exchange_declare(exchange=self.exchange_name, exchange_type='direct')
+
+            #cola a asociar
+            result = self.channel.queue_declare(queue='', exclusive=True)
+            self.queue_name = result.method.queue
+
+            #bind
+            for routing_key in self.routing_keys:
+                self.channel.queue_bind(exchange=self.exchange_name, 
+                                        queue=self.queue_name, routing_key=routing_key)
+                
+        except pika.exceptions.AMQPConnectionError as e:
+            raise MessageMiddlewareDisconnectedError() from e 
+
+        except pika.exceptions.AMQPError as e:
+            raise MessageMiddlewareMessageError() from e 
+
+    # Wrapper para invocar el start_consuming dado que la funcion de callback no tiene los parametros 
+    # requeridos por pika
+    def wrap_callback(self, on_message_callback):
+
+        def callback(ch, method, properties, body):
+
+            def ack():
+                self.channel.basic_ack(delivery_tag=method.delivery_tag)
+
+            def nack():
+                self.channel.basic_nack(delivery_tag=method.delivery_tag, requeue=True)
+
+            on_message_callback(body, ack, nack)
+
+        return callback
+
+    #Comienza a escuchar a la cola/exchange e invoca a on_message_callback tras
+    #cada mensaje de datos o de control con el cuerpo del mensaje.
+    # on_message_callback tiene como parámetros:
+    # message - El valor tal y como lo recibe el método send de esta clase.
+    # ack - Función que al invocarse realiza ack al mensaje que se está consumiendo.
+    # nack - Función que al invocarse realiza nack al mensaje que se está consumiendo. 
+    #Si se pierde la conexión con el middleware eleva MessageMiddlewareDisconnectedError.
+    #Si ocurre un error interno que no puede resolverse eleva MessageMiddlewareMessageError.
+    def start_consuming(self, on_message_callback):
+
+        try:
+
+            self.channel.basic_consume(queue=self.queue_name, 
+                                        on_message_callback=self.wrap_callback(on_message_callback))
+
+            self.channel.start_consuming()
+
+        except pika.exceptions.AMQPConnectionError as e:
+            raise MessageMiddlewareDisconnectedError() from e 
+            
+        except pika.exceptions.AMQPError as e:
+            raise MessageMiddlewareMessageError() from e 
+            # resta ver si hay un caso genuino de error que no es error a considerar
+        
+    #Si se estaba consumiendo desde la cola/exchange, se detiene la escucha. Si
+    #no se estaba consumiendo de la cola/exchange, no tiene efecto, ni levanta
+    #Si se pierde la conexión con el middleware eleva MessageMiddlewareDisconnectedError.
+    def stop_consuming(self):
+        
+        try:
+
+            self.channel.stop_consuming()
+
+        except pika.exceptions.AMQPConnectionError as e:
+            raise MessageMiddlewareDisconnectedError() from e 
+        
+    #Envía un mensaje a la cola o al tópico con el que se inicializó el exchange.
+    #Si se pierde la conexión con el middleware eleva MessageMiddlewareDisconnectedError.
+    #Si ocurre un error interno que no puede resolverse eleva MessageMiddlewareMessageError.
+    def send(self, message):
+
+        try:
+
+            self.channel.basic_publish(
+                exchange=self.exchange_name,
+                routing_key=self.routing_keys[0],
+                body=message,
+                properties=pika.BasicProperties(delivery_mode=pika.DeliveryMode.Persistent)
+                )
+
+        except pika.exceptions.AMQPConnectionError as e:
+            raise MessageMiddlewareDisconnectedError() from e 
+    
+        except pika.exceptions.AMQPError as e:
+            raise MessageMiddlewareMessageError() from e 
+        
+    
+    #Se desconecta de la cola o exchange al que estaba conectado.
+    #Si ocurre un error interno que no puede resolverse eleva MessageMiddlewareCloseError.
+    def close(self):
+
+        try:
+            #orden inverso al del init, no se borra la cola porque se comparte
+            self.channel.close()
+
+            self.connection.close()
+
+        except Exception as e:
+            raise MessageMiddlewareCloseError() from e
